b735e5c8802ebea4915055632f884d98c56cf7b2
Nishkarsh Kumar <59024538+kumarnishkarsh@users.noreply.github.com>
2021-02-16 15:18:11 +0530

    Add files via upload

---
+++ Python/Final Exams ICT Practical Q2..py
@@ -0,0 +1,30 @@
+print('Name: Nishkarsh Kumar')
+print('Class: 7 Deneb')
+print('Roll no.: 21')
+print('Date: 16-02-2021')
+
+print('Q2. Write a program to print cube of the numbers given in range.')
+print('Answer: ')
+
+num_1 = int(input('Enter the 1st number in range:'))
+num_2 = int(input('Enter the 2nd number in range:'))
+
+for i in range(num_1,num_2):
+    print(i*i*i)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python/Final Exams ICT Practical Q4..py
@@ -0,0 +1,17 @@
+print('Name: Nishkarsh Kumar')
+print('Class: 7 Deneb')
+print('Roll no.: 21')
+print('Date: 16-02-2021')
+
+print('Q4. Write a program to check if a number input is a positive or negative number.')
+print('Answer: ')
+
+num = int(input('Enter the number:'))
+
+if num<0 :
+    print(num , 'is negative.')
+elif num==0 :
+    print('0 is neither positive nor negative.')
+else:
+    print(num, 'is positive.')
+    
